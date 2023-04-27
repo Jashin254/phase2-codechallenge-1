bbb7fa04e5904035b9158c088004e69d990bc959
Jashin254 <joshuanissi254@gmail.com>
2023-04-27 11:36:53 +0300

    challenge completed

--- flatiron-bank/src/Components/table.js
+++ flatiron-bank/src/Components/table.js
@@ -3,14 +3,34 @@ import React from 'react'
 import { useState , useEffect } from 'react';
 
 function TransTable(){
-    // const [selectedTransactions, setSelectedTransactions] = useState("All");
+    
  const [Transactions, setTransactions] = useState([]);
+ const [searchTerm, setSearchTerm] = useState("");
+
     useEffect(() => 
  {
    fetch("http://localhost:3000/Transactions")
      .then((r) => r.json())
      .then((data) => setTransactions(data));
  }, [])
+ const handleDelete = id => {
+  // Filter out the deleted transaction from the array
+  const newData = Transactions.filter(transaction => transaction.id !== id);
+  setTransactions(newData);
+  // Send a DELETE request to the json file to delete the transaction
+  fetch(`http://localhost:3000/transactions/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+};
+const filteredData = Transactions.filter(transaction =>
+  transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+);
     return (<div className="Trans"
     style={{
       textAlign: "center",
@@ -23,7 +43,13 @@ function TransTable(){
       fontSize:"35px"
       }}>
     <h1>Transactions</h1>
-    
+    <input
+        type="text"
+        placeholder="Search by description"
+        value={searchTerm}
+        onChange={e => setSearchTerm(e.target.value)}
+      />
+
      <table>
       <thead>
         <tr>
@@ -42,6 +68,7 @@ function TransTable(){
       </thead>
       <tbody>
         {Transactions.map((Transactions) => (
+        
           <tr key={Transactions.id}>
 
            
@@ -53,6 +80,9 @@ function TransTable(){
            <td>{Transactions.category}</td>
                
            <td>{Transactions.amount}</td>
+           <td>   <button onClick={() => handleDelete(Transactions.id)}>
+                  Delete
+                </button></td>
           </tr>
         ))}
       </tbody>
